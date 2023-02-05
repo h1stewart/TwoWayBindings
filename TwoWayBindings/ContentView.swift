af5ba06ce7ea0fdacea1790071edf755c277baf3
Hailey Stewart <kickinhailey@icloud.com>
2023-02-05 15:41:08 -0500

    Video 1.21

--- TwoWayBindings/ContentView.swift
+++ TwoWayBindings/ContentView.swift
@@ -7,14 +7,53 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var count=0 //State variables are tucked away and remain as the rest of the view is reconstructed
+    @State private var toggleIsOn = true
+    @State private var name = ""
+    @State private var selectedColor: Color = .red
+    @State private var selectedDate = Date()
+    @State private var stepperValue = 1
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Group{
+                Text("Pressed Count: \(count)")
+                Button("Press Me"){
+                    count += 1
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Spacer()
+                
+                TextField("Enter a name", text: $name)
+                    .textFieldStyle(.roundedBorder)
+                Text("Name entered: \(name)")
+                
+                Spacer()
+                
+                Toggle("Toggle is \(toggleIsOn ? "on" : "off"): ", isOn: $toggleIsOn)
+                
+                Spacer()
+            }
+            Rectangle()
+                .fill(selectedColor)
+                .frame(width:100, height: 100)
+            ColorPicker("Pick a rectangle color:", selection: $selectedColor)
+            
+            Spacer()
+            
+            DatePicker("Date:", selection: $selectedDate)
+            Text("Selected Date is: \(selectedDate.formatted(date: .abbreviated, time: .shortened))")
+            
+            Spacer()
+            
+            Stepper("Stepper value: \(stepperValue)", value: $stepperValue, in: 1...10)
+            
+            Spacer()
         }
+        .font(.title2)
         .padding()
     }
 }
